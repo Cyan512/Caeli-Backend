a3100aa371ca8627ae661d090b9ecf5876b22a14
Erick Uscachi <182935@unsaac.edu.pe>
2025-07-13 18:37:42 -0500

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/Caeli/CaeliApplication.java
@@ -0,0 +1,13 @@
+package com.example.Caeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaeliApplication.class, args);
+	}
+
+}
